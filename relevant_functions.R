405f3ddfcc2c129807f9aa4a1c9f34221d40cf28
Innocent Gerald Asiimwe <innocentgerald@yahoo.co.uk>
2022-09-22 08:00:06 +0100

    Make spacing between if(, ){ etc consistent.

--- relevant_functions.R
+++ relevant_functions.R
@@ -10,7 +10,7 @@
 # Output: a UKBiobank dataset with more rows (one participant has multiple rows that correspond to 
 #           the different instances/assessments) and fewer columns (each variable is present in its
 #           own column).
-ukb_reshape_long <- function(data, dict){
+ukb_reshape_long <- function(data, dict) {
   fields <- data %>% 
     colnames() %>% 
     str_extract_all(., "f.[0-9]*") %>% 
@@ -21,7 +21,7 @@ ukb_reshape_long <- function(data, dict){
     na.omit()
   fields <- append(fields[!fields %in% 53], 53, after = 0) # remove date field, so that it can be added  
           # to the start of the vector - this enables all assessments to be added using left_join().
-  for(k in seq_along(fields)){
+  for (k in seq_along(fields)) {
     columns <- unlist(str_extract_all(colnames(data), 
                                       str_c("f.", fields[k], ".[0-9]*.[0-9]*", 
                                             sep=""))) # columns to pivot into longer format
@@ -36,7 +36,7 @@ ukb_reshape_long <- function(data, dict){
       arrange(f.eid) %>%
       mutate(I = str_replace(.$I, str_c("f.", fields[k], sep=""), "")) %>% 
       mutate(I = unlist(str_extract(.$I, ".[0-9].")))
-    if(k == 1) 
+    if (k == 1) 
       data_long <- dataQ 
     else 
       data_long <- left_join(data_long, dataQ)   #Join by "f.eid" and "I"
@@ -61,7 +61,7 @@ ukb_min_sample <- function(effect, std_ev, p = 5e-8, power = 0.8, maf = 0.2, n_t
                                      power = power)$n)
                          * 2) # sample size assuming equally-sized groups (i.e. MAF = 50%)
   unequal_sizes <- equal_sizes * n_times # assuming having unequally-sized group will increase sample size by at most n_times
-  ukb_power_diff <- function(sample_size){
+  ukb_power_diff <- function(sample_size) {
     current_power <- (pwr.t2n.test(n1 = maf * sample_size, 
                                    n2 = (1 - maf) * sample_size, 
                                    d = effect / std_ev, 
@@ -85,8 +85,8 @@ ukb_min_sample <- function(effect, std_ev, p = 5e-8, power = 0.8, maf = 0.2, n_t
 #           bendroflumethiazide-treated participants.
 # Output: a dataset containing IDs to be included (of the related individuals, those with lower 
 #         missingness levels).
-ukb_related <- function(related.df, missingness.df, bd_BFZ.df){
-  ukb_missingness <- function(id, missingness.df){
+ukb_related <- function(related.df, missingness.df, bd_BFZ.df) {
+  ukb_missingness <- function(id, missingness.df) {
     colnames(missingness.df)[1:2] <- c("ID1", "missingness")
     id_missing <- missingness.df %>% 
       filter(ID1 == id) %>% 
@@ -102,7 +102,7 @@ ukb_related <- function(related.df, missingness.df, bd_BFZ.df){
   ids <- ids[ids %in% bd_BFZ.df$f.eid]
   if (length(ids) > 0) {
     output <- vector("double", length(ids))
-    for(i in seq_along(ids)) output[[i]] <- ukb_missingness(ids[[i]], missingness.df)
+    for (i in seq_along(ids)) output[[i]] <- ukb_missingness(ids[[i]], missingness.df)
     return(as.character(ids[[which.min(output)]]))
   } else {
     return("not_in_bd_BFZ")
@@ -114,7 +114,7 @@ ukb_related <- function(related.df, missingness.df, bd_BFZ.df){
 # Aim: change "Prefer not to answer" to NA.      
 # Input(s): factor with or without the level "Prefer not to answer".
 # Output: factor with "Prefer not to answer" entries changed to NA.
-ukb_recode_factor_na <- function(x){
+ukb_recode_factor_na <- function(x) {
   for (i in seq_along(x)) {
     x[[i]] <- x[[i]] %>% recode_factor("Prefer not to answer" = NA_character_)
   }
@@ -130,7 +130,7 @@ ukb_recode_factor_na <- function(x){
 # Output: formatted means (with sds), medians (with IQRs and ranges), and percentages. If missing 
 #         data exists, the percentage of missing data is also outputted. 
 ukb_statistics <- function(dataQ, f, round_digits = 1) {
-  if(f == "mean") {
+  if (f == "mean") {
     statistic <- paste(format(round(mean(dataQ, na.rm = TRUE), round_digits), nsmall = round_digits), 
                        " (", 
                        format(round(sd(dataQ, na.rm = TRUE), round_digits), nsmall = round_digits), 
@@ -138,7 +138,7 @@ ukb_statistics <- function(dataQ, f, round_digits = 1) {
                        sep = ""
     )
     return(statistic)
-  } else if(f == "median") {
+  } else if (f == "median") {
     quantiles <- format(round(quantile(dataQ, na.rm = TRUE), digits = round_digits), 
                         nsmall = round_digits)
     statistic <- paste(quantiles[[3]],
@@ -153,7 +153,7 @@ ukb_statistics <- function(dataQ, f, round_digits = 1) {
                        ")",
                        sep = ""
     )
-    if(TRUE %in% is.na(dataQ)) {
+    if (TRUE %in% is.na(dataQ)) {
       percent_na <- paste(sum(is.na(dataQ)),
                           " (",
                           format(round(sum(is.na(dataQ)) * 100 / length(dataQ), round_digits), 
@@ -164,7 +164,7 @@ ukb_statistics <- function(dataQ, f, round_digits = 1) {
       statistic <- c(statistic, percent_na)
     }
     return(statistic)
-  } else if(f == "percentage") {
+  } else if (f == "percentage") {
     out <- vector("double", length(levels(dataQ)))
     for (i in seq_along(levels(dataQ))) {
       out[[i]] <- paste(table(dataQ)[i],
@@ -176,7 +176,7 @@ ukb_statistics <- function(dataQ, f, round_digits = 1) {
                         sep = ""
       )
     }
-    if(TRUE %in% is.na(dataQ)) {
+    if (TRUE %in% is.na(dataQ)) {
       percent_na <- paste(sum(is.na(dataQ)), 
                           " (",
                           format(round(sum(is.na(dataQ)) * 100 / length(dataQ), 
@@ -199,18 +199,18 @@ ukb_statistics <- function(dataQ, f, round_digits = 1) {
 # Input(s): dataset with numerical vectors and/or factors.
 #         : number of requires decimal places (default 1).
 # Output: dataset with three columns (variable, statistic, and value).
-ukb_summary <- function(data, round_digits = 1){
+ukb_summary <- function(data, round_digits = 1) {
   names_list <- colnames(data)
   output <- vector("list", length(names_list))
-  for(i in seq_along(names_list)) {
+  for (i in seq_along(names_list)) {
     name_list <- names_list[[i]]
     dataQ <- data[[name_list]] # or: data %>% select(name_list)
-    if(is.numeric(dataQ) == TRUE){
+    if (is.numeric(dataQ) == TRUE) {
       values1 <- ukb_statistics(dataQ, f = "mean", round_digits)
       values2 <- ukb_statistics(dataQ, f = "median", round_digits)
       name_levels <- rep(name_list, 2)
       statistic <- c("mean (sd)", "median (IQR; range)")
-      if(TRUE %in% is.na(dataQ)) {
+      if (TRUE %in% is.na(dataQ)) {
         name_levels <- c(name_levels, paste(name_list, "missing", sep = ", "))
         statistic <- c(statistic, "n (%)")
       }
@@ -218,9 +218,9 @@ ukb_summary <- function(data, round_digits = 1){
                                  statistic = statistic,
                                  value = c(values1, values2)
       )
-    } else if(is.factor(dataQ) == TRUE){
+    } else if (is.factor(dataQ) == TRUE) {
       name_levels <- levels(dataQ)
-      if(TRUE %in% is.na(dataQ)) name_levels <- c(name_levels, "missing")
+      if (TRUE %in% is.na(dataQ)) name_levels <- c(name_levels, "missing")
       values <- ukb_statistics(dataQ, f = "percentage", round_digits)
       variable_summary <- tibble(variable = paste(name_list, 
                                                   name_levels, sep = ", "),
@@ -236,8 +236,8 @@ ukb_summary <- function(data, round_digits = 1){
     output[[i]] <- variable_summary
   }
   
-  for(i in seq_along(output)){
-    if(i == 1) {
+  for (i in seq_along(output)) {
+    if (i == 1) {
       out <- output[[i]]
     } else {
       out <- rbind(out, output[[i]])
@@ -256,13 +256,13 @@ ukb_summary <- function(data, round_digits = 1){
 #         : number of requires decimal places (default is 4).
 # Output: T-test for means; Mann Whitney for medians; and, Fisher's Exact Test (N < 1000) and 
 #         Chi-square (N ≥ 1000) for categorical variables.
-ukb_p_value <- function(all_data, included, excluded, round_digits = 4){
+ukb_p_value <- function(all_data, included, excluded, round_digits = 4) {
   out <- vector("list", length(included))
-  for (x in seq_along(included)){
+  for (x in seq_along(included)) {
     variable <- colnames(included)[[x]]
     index <- sapply(str_split(all_data$variable, ", "), `[`, 1) == variable
     variable_data <- all_data[index, ]
-    if(is.numeric(included[[variable]]) == TRUE) {
+    if (is.numeric(included[[variable]]) == TRUE) {
       t_test_mean <- round(t.test(included[[variable]],
                                   excluded[[variable]])$p.value, 
                            round_digits)
@@ -270,7 +270,7 @@ ukb_p_value <- function(all_data, included, excluded, round_digits = 4){
                                                     excluded[[variable]])$p.value, 
                                         round_digits)
       out[[x]] <- c(t_test_mean, mann_whitney_test_median)
-    } else if(is.factor(included[[variable]]) == TRUE) {
+    } else if (is.factor(included[[variable]]) == TRUE) {
       analysis_matrix <- matrix(c(as.numeric(
         sapply(str_split(variable_data$included, " "), `[`, 1)),
         as.numeric(sapply(str_split(variable_data$excluded, " "), `[`, 1))), 
@@ -413,8 +413,8 @@ ukb_bar_plots <- function(bd_BFZ, covariate) {
     )
 }
 
-ukb_qq_plots <- function(data, race = NULL){
-  for (i in seq_along(colnames(data))){
+ukb_qq_plots <- function(data, race = NULL) {
+  for (i in seq_along(colnames(data))) {
     data_covariate <- data[,i]
     data_covariate <- data_covariate[!is.na(data_covariate)]
     qqnorm(data_covariate, pch = 1, frame = FALSE,
@@ -424,7 +424,7 @@ ukb_qq_plots <- function(data, race = NULL){
   }
 }
 
-ukb_manhattan_plots <- function(data, race, outcome){
+ukb_manhattan_plots <- function(data, race, outcome) {
   y_limit <- max(ceiling(-log10(5e-8)), ceiling(-log10(min(data$P))))
   png(paste("UKBB_", race, "_", outcome, "_manhattan.png", sep = ""), 
       width = 1500, height = 800, res = 120)
@@ -436,7 +436,7 @@ ukb_manhattan_plots <- function(data, race, outcome){
   dev.off()
 } 
 
-ukb_qq_gwas_plots <- function(data, race, outcome){
+ukb_qq_gwas_plots <- function(data, race, outcome) {
   y_limit <- max(ceiling(-log10(5e-8)), ceiling(-log10(min(data$P))))
   png(paste("UKBB_", race, "_", outcome, "_qqplot.png", sep = ""), 
       width = 1500, height = 1500, res = 120)
@@ -452,9 +452,9 @@ ukb_qq_gwas_plots <- function(data, race, outcome){
 # Aim: code categorical variables to either "1" or "2", a format that SNPTest requires.    
 # Input(s): factors with two levels.
 # Output: vector with 1s and 2s.
-ukb_recode_snptest <- function(x){
+ukb_recode_snptest <- function(x) {
   for (y in names(x)) {
-    x[y] <- ifelse(x[[y]] == 0, 1, 2)
+    x[y] <- ifelse (x[[y]] == 0, 1, 2)
   }
   return(x)
 }
@@ -494,16 +494,16 @@ ukb_colour_format <- function(x, color) {
 #           are assumed to have been computed earlier. If not, include them in the environment.
 # Output: ukb_hux outputs a huxtable for participant characteristics stratified by the three races 
 #         while ukb_hux_2 shows those included and excluded from a specific analysis (race-stratified).
-ukb_set_contents <- function(ht, ht_row, ht_cols, value){
+ukb_set_contents <- function(ht, ht_row, ht_cols, value) {
   i <- 1
-  while(i <= length(ht_cols)){
+  while (i <= length(ht_cols)) {
     ht <- ht %>% set_contents(ht_row, ht_cols[[i]], value[[i]])
     i <- i + 1
   }
   return(ht)
 }
-ukb_set_merge_contents <- function(ht, ht_rows, ht_cols){
-  for(i in seq_along(ht_cols)){
+ukb_set_merge_contents <- function(ht, ht_rows, ht_cols) {
+  for (i in seq_along(ht_cols)) {
     ht_col <- ht_cols[[i]]
     nrows <- max(ht_rows) - min(ht_rows) + 1
     ht <- ht %>% 
@@ -514,17 +514,17 @@ ukb_set_merge_contents <- function(ht, ht_rows, ht_cols){
                         )
                    ) %>% 
       merge_cells(ht_rows, ht_col)
-    if(i == 1) ht_all <- ht[, 1:min(ht_col)] else ht_all <- add_columns(ht_all, ht[, ht_col])
+    if (i == 1) ht_all <- ht[, 1:min(ht_col)] else ht_all <- add_columns(ht_all, ht[, ht_col])
   }
   return(ht_all)
 }
 
-ukb_hux <- function(race_summary_all, covariate_list, covariate_list_2){
-  for (x in seq_along(covariate_list)){
+ukb_hux <- function(race_summary_all, covariate_list, covariate_list_2) {
+  for (x in seq_along(covariate_list)) {
     variable <- covariate_list[[x]]
     index <- sapply(str_split(race_summary_all[[1]], ", "), `[`, 1) == variable
     variable_data <- race_summary_all[index, ]
-    if(nrow(variable_data) > 0){
+    if (nrow(variable_data) > 0) {
       hux_values <- c("Variables", paste0(c("White", "Black", "Asian"),
                                           " (<i>N</i> = ",
                                           c(ukb_comma(analyzed_white),
@@ -545,7 +545,7 @@ ukb_hux <- function(race_summary_all, covariate_list, covariate_list_2){
           merge_cells(2:3, 2) %>%
           set_contents(2, 1, covariate_list_2[[x]]) %>%
           ukb_set_merge_contents(2:3, 3:5)
-        if(TRUE %in% str_detect(variable_data$statistic, "mean")){
+        if (TRUE %in% str_detect(variable_data$statistic, "mean")) {
           hux_var <- hux_var_2 %>%
             set_contents(2, 2, "Mean (SD)\nMedian (IQR; range)")
           } else {
@@ -554,7 +554,7 @@ ukb_hux <- function(race_summary_all, covariate_list, covariate_list_2){
             set_contents(2, 2, paste(factor_levels, collapse = "\n"))
           }
       } else{
-        if(TRUE %in% str_detect(variable_data$statistic, "mean")) {
+        if (TRUE %in% str_detect(variable_data$statistic, "mean")) {
           hux_var <- hux_header %>%
             merge_cells(2:4, 1) %>%
             merge_cells(2:4, 2) %>%
@@ -571,19 +571,19 @@ ukb_hux <- function(race_summary_all, covariate_list, covariate_list_2){
             ukb_set_merge_contents(2:5, 3:5)
         }
       }
-      if(x == 1) hux_all <- hux_var else hux_all <- add_rows(hux_all, hux_var[-1,])
+      if (x == 1) hux_all <- hux_var else hux_all <- add_rows(hux_all, hux_var[-1,])
     }
     }
   return(hux_all)
 }
 
-ukb_hux_2 <- function(data, covariate_list, covariate_list_2, outcome){
+ukb_hux_2 <- function(data, covariate_list, covariate_list_2, outcome) {
   data <- data[[outcome]]
-  for (x in seq_along(covariate_list)){
+  for (x in seq_along(covariate_list)) {
     variable <- covariate_list[[x]]
     index <- sapply(str_split(data[[1]], ", "), `[`, 1) == variable
     variable_data <- data[index, ]
-    if(nrow(variable_data) > 0){
+    if (nrow(variable_data) > 0) {
       hux_values <- c("Variables", paste0(c("White", "Black", "Asian"),
                                           " (<i>N</i> = ",
                                           c(ukb_comma(analyzed_white),
@@ -615,7 +615,7 @@ ukb_hux_2 <- function(data, covariate_list, covariate_list_2, outcome){
           merge_cells(3:4, 2) %>%
           set_contents(3, 1, covariate_list_2[[x]]) %>%
           ukb_set_merge_contents(3:4, 3:11)
-        if(TRUE %in% str_detect(variable_data$statistic, "mean")){
+        if (TRUE %in% str_detect(variable_data$statistic, "mean")) {
           hux_var <- hux_var_2 %>%
             set_contents(3, 2, "Mean (SD)\nMedian (IQR; range)")
         } else {
@@ -632,7 +632,7 @@ ukb_hux_2 <- function(data, covariate_list, covariate_list_2, outcome){
           set_contents(3, 2, paste(factor_levels, collapse = "\n")) %>%
           ukb_set_merge_contents(3:5, 3:11) 
       }
-      if(x == 1) hux_all <- hux_var else hux_all <- add_rows(hux_all, hux_var[-1:-2,])
+      if (x == 1) hux_all <- hux_var else hux_all <- add_rows(hux_all, hux_var[-1:-2,])
     }
   }
   hux_all <- hux_all %>%
@@ -657,7 +657,7 @@ ukb_subset_count <- function(bd_BFZ_df, specific_race, covariate = NULL) {
 # Aim: compute the genomic inflation factor (lambda).     
 # Input(s): gwas results
 # Output: lambda.
-ukb_lambda <- function(data){
+ukb_lambda <- function(data) {
   chisq = qchisq(data$P, 1, lower.tail = FALSE)
   return(median(chisq) / qchisq(0.5, 1))
 }
@@ -670,7 +670,7 @@ ukb_lambda <- function(data){
 #           already exists.
 # Output: data frame (.csv and .rds files) that contains associated genes as well 
 #         as SNP locations (e.g. introns) 
-ukb_dbsnp <- function(race, outcome, n_digits = 5){
+ukb_dbsnp <- function(race, outcome, n_digits = 5) {
   file <- fread(paste0("UKBB_", race, "_", outcome, ".csv")) 
   N <- unique(file$all_total)
   file <- file %>%
@@ -703,7 +703,7 @@ ukb_dbsnp <- function(race, outcome, n_digits = 5){
     select(SNP:Position, Gene, `Functional consequence`, `Reference allele`:`Info score`,
            all_maf, `Beta (SE)`, `P value`)
   colnames(file)[which(colnames(file) == "all_maf")] <- paste0("MAF (N = ", N, ")")
-  for(k in 1:nrow(file)) {
+  for (k in 1:nrow(file)) {
     snp <- file$SNP[k]
     if (str_detect(snp, ":") == FALSE) {
       SNP_info <- paste0("https://www.ncbi.nlm.nih.gov/snp/?term=", snp) %>%
@@ -725,7 +725,7 @@ ukb_dbsnp <- function(race, outcome, n_digits = 5){
         gsub("prime", "-prime", .)
     }
     message(paste(round(k / nrow(file) * 100, 3), "% complete", sep = ""))
-    if(k %% 100 == 0) {Sys.sleep(10)} # Pause for 10 seconds after every 100 requests
+    if (k %% 100 == 0) {Sys.sleep(10)} # Pause for 10 seconds after every 100 requests
   }
   write.csv(file,  file = paste0("UKBB_", race, "_", outcome, "_final.csv"), row.names = FALSE)
   write_rds(file,  file = paste0("UKBB_", race, "_", outcome, "_final.rds"))
@@ -740,7 +740,7 @@ ukb_image_annotate <- function(image, text, x = 0, y = 0,  size = 20, color = "b
                                boxcolor = "transparent", degrees = 0, font = 'Times',
                                style = "italic", bold = FALSE, decoration = NULL, kerning = 0)
 {
-  if(bold == FALSE) weight = 400 else weight = 700 
+  if (bold == FALSE) weight = 400 else weight = 700 
   image_annotate(image = image, text = text, size = size, color = color, boxcolor = boxcolor, degrees = degrees, 
                  location = paste0("+", x, "+", y), font = font, style = style, 
                  weight = weight, decoration = decoration, kerning = kerning)
